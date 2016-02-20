042f6d8bdd845c406822850db2ef9bd7a0f0b5ae
cfmdobbie <cfmdobbie@gmail.com>
2016-02-20 10:59:33 +0000

    SVG utility function for making a <path> element to denote an event range (first cut, currently unused)

--- svg_utils.js
+++ svg_utils.js
@@ -1,3 +1,19 @@
+// Utility function to make a horizontal brace for denoting a range
+function makeHorizontalBrace(start, end, y) {
+  var path = document.createElementNS('http://www.w3.org/2000/svg','path');
+  var move = "m " + start.toFixed(2) + "," + y.toFixed(2);
+  var curveNW = " c 0,-5 5,-5 5,-5";
+  var curveSE = " c 5,0 5,-5 5,-5";
+  var curveSW = " c 0,5 5,5 5,5";
+  var curveNE = " c 5,0 5,5 5,5";
+  var lineLength = (end - start - 20) / 2;
+  var line = " l " + lineLength.toFixed(2) + ",0";
+  var d = move + curveNW + line + curveSE + curveSW + line + curveNE;
+  path.setAttribute('d', d);
+  path.setAttribute('style', 'fill: none; stroke: black; stroke-width: 1px;');
+  return path;
+}
+
 // Utility function to generate an SVG <line>
 function makeLine(x1, y1, x2, y2, color, lineWidth) {
   var e = document.createElementNS('http://www.w3.org/2000/svg','line');
